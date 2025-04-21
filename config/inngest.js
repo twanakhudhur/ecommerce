85d6f885b9ea590b1bc4b2eb2d3c9ecc91b88392
twanakhudhur <twana.brno@gmail.com>
2025-04-21 17:50:35 +0200

    first ommit

---
+++ config/inngest.js
@@ -0,0 +1,61 @@
+import { Inngest } from "inngest";
+import dbConnect from "./db";
+import User from "@/models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({
+  id: "tkEcommerce-next",
+});
+
+// Inngest function to save the user to the database
+export const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+  },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      name: `${first_name} ${last_name}`,
+      email: email_addresses[0].email_address,
+      imageUrl: image_url,
+    };
+    await dbConnect();
+    await User.create(userData);
+  }
+);
+
+// Inngest function to update the user in the database
+export const syncUserUpdation = inngest.createFunction(
+  {
+    id: "update-user-from-clerk",
+  },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      name: `${first_name} ${last_name}`,
+      email: email_addresses[0].email_address,
+      imageUrl: image_url,
+    };
+    await dbConnect();
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
+
+// Inngest function to delete the user from the database
+export const syncUserDeletion = inngest.createFunction(
+  {
+    id: "delete-user-from-clerk",
+  },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+    await dbConnect();
+    await User.findByIdAndDelete(id);
+  }
+);
